6cf8b88bd6dcf6156b1dfb82d04419b35c8d248d
Adam Perhala <perhala.3@buckeyemail.osu.edu>
2022-09-19 13:00:06 -0400

    add ConnectToServerCommand class

---
+++ Client/ConnectToServerCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    internal class ConnectToServerCommand : ICommand
+    {
+        private Game1 game;
+        ConnectToServerCommand(Game1 game)
+        {
+            this.game = game;
+        }
+        public void Execute()
+        {
+            this.game.client = new Client();
+            this.game.client.StartClient();
+            this.game.updateables.Add(this.game.client);
+        }
+    }
+}
