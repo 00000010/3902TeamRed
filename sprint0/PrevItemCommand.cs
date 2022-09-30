b2f297b93d7d642282f50cf6691e1e6018d8b674
Adam Perhala <perhala.3@buckeyemail.osu.edu>
2022-09-30 18:43:57 -0400

    add Item commands

---
+++ sprint0/PrevItemCommand.cs
@@ -0,0 +1,30 @@
+﻿using sprint0;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace sprint0
+{
+    public class PrevItemCommand : ICommand
+    {
+        private Game1 game;
+        public PrevItemCommand(Game1 game)
+        {
+            this.game = game;
+        }
+        public void Execute()
+        {
+            game.currItemIndex--;
+            if (game.currItemIndex < 0)
+            {
+                game.currItemIndex = game.items.Count-1;
+            }
+            game.item.SourceRectangle = game.items[game.currItemIndex].SourceRectangle;
+            game.item.Texture = game.items[game.currItemIndex].Texture;
+        }
+    }
+}
+
