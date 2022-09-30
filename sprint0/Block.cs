5e7fa48e3e288d26894465067021723af182e70f
Adam Perhala <perhala.3@buckeyemail.osu.edu>
2022-09-30 18:41:32 -0400

    add Block class

---
+++ sprint0/Block.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sprint0
+{
+    public class Block : Sprite
+    {
+        
+        public Vector2 Velocity { get; set; }
+        public Block(Texture2D texture, Rectangle[] sourceRectangle, SpriteBatch spriteBatch, Vector2 position)
+            : base(texture, sourceRectangle, spriteBatch, position)
+        {
+            Velocity = Vector2.Zero;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            UpdateVelocity(gameTime);
+            UpdatePosition(gameTime);
+            UpdateFrame(gameTime);
+        }
+
+        private void UpdateFrame(GameTime gameTime)
+        {
+            Frame++;
+            if (Frame >= SourceRectangle.Length)
+            {
+                Frame = 0;
+            }
+        }
+
+        private void UpdatePosition(GameTime gameTime)
+        {
+            Position += Velocity;
+        }
+
+        private void UpdateVelocity(GameTime gameTime)
+        {
+            // no-op
+        }
+    }
+}
+
