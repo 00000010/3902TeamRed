9b7e2254da75b6de6948218385828cc3b96bb170
owenhennessey <obuddyblueandwhite@gmail.com>
2022-10-22 03:43:26 -0400

    Added artificial level 18

---
+++ sprint0/Game/LoadLevelCommand.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace sprint0
+{
+    internal class LoadLevelCommand : ICommand
+    {
+        private Game1 game;
+        private LevelLoader loader;
+
+        public LoadLevelCommand(Game1 game)
+        {
+            this.game = game;
+        }
+
+        public void Execute()
+        {
+            loader = game.loader;
+            loader.LoadNextLevel();
+        }
+    }
+}
