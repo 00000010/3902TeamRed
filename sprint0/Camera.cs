eb201176646d24fd1ef6cef1922b7013908d8469
owenhennessey <obuddyblueandwhite@gmail.com>
2022-11-09 13:29:53 -0500

    Commit

---
+++ sprint0/Camera.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Numerics;
+using Microsoft.Xna.Framework;
+
+namespace sprint0
+{
+    internal class Camera : ICamera, IObject
+    {
+        private GameCamera gameCamera;
+
+        public Sprite Sprite { get; set; }
+        public Microsoft.Xna.Framework.Vector2 Position { get { return Sprite.Position; } set { Sprite.Position = value; } }
+        public Microsoft.Xna.Framework.Vector2 Velocity { get { return Sprite.Velocity; } set { Sprite.Velocity = value; } }
+        public Direction Direction { get; set; }
+
+        public bool Enabled => throw new NotImplementedException();
+
+        public int UpdateOrder => throw new NotImplementedException();
+
+        public int DrawOrder => throw new NotImplementedException();
+
+        public bool Visible => throw new NotImplementedException();
+
+        public Camera() { }
+
+        public event EventHandler<EventArgs> EnabledChanged;
+        public event EventHandler<EventArgs> UpdateOrderChanged;
+        public event EventHandler<EventArgs> DrawOrderChanged;
+        public event EventHandler<EventArgs> VisibleChanged;
+
+        public Camera(GameCamera camera)
+        {
+            gameCamera = camera;
+        }
+
+        // Set camera to proper position and ensure it is there correctly
+        public bool GetPosition(System.Numerics.Vector2 position, out System.Numerics.Vector2 newPosition)
+        {
+            newPosition = gameCamera.WorldToScreenPosition(position);
+            return gameCamera.isInScreen(position);
+        }
+        // Check camera is within the screen
+        public bool InsideScreen(System.Numerics.Vector2 position)
+        {
+            return gameCamera.isInScreen(position);
+        }
+        // Move camera to new position
+        public void MoveCamera(Direction direction)
+        {
+            gameCamera.moveCamera(direction);
+        }
+        // Check if the camera is transitioning or not to prevent multiple at the same time
+        public bool CameraInTransition()
+        {
+            return gameCamera.isLocked;
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+            Sprite.Update(gameTime);
+        }
+
+        public virtual void Draw(GameTime gameTime)
+        {
+            Sprite.Draw(gameTime);
+        }
+    }
+}
---
+++ sprint0/GameCamera.cs
@@ -0,0 +1,82 @@
+﻿using System.Drawing;
+using System.Numerics;
+
+namespace sprint0
+{
+    public class GameCamera
+    {
+        private float velocity;
+        private Vector2 currentPosition;
+        private Vector2 futurePosition;
+        private Direction movementDirection;
+        public bool isLocked { get { return (int)currentPosition.X != (int)futurePosition.X || (int)currentPosition.Y != (int)futurePosition.Y; } }
+
+        public Vector2 Position { get { return currentPosition; } }
+        public Vector2 TilePosition { get { return new Vector2(currentPosition.X / Constants.SCREEN_WIDTH, currentPosition.Y / Constants.SCREEN_HEIGHT); } }
+
+        public GameCamera()
+        {
+            velocity = 5f;
+            currentPosition = new Vector2(0, 0);
+        }
+
+        public void Update(double gameTime)
+        {
+            if (!isLocked)
+                return;
+
+            if (currentPosition.X < futurePosition.X)
+                currentPosition.X += velocity;
+            if (currentPosition.X > futurePosition.X)
+                currentPosition.X -= velocity;
+            if (currentPosition.Y > futurePosition.Y)
+                currentPosition.Y -= velocity;
+            if (currentPosition.Y < futurePosition.Y)
+                currentPosition.Y += velocity;
+
+            //if (ManagerFunction.Distance(currentPosition, futurePosition) < 5)
+            //{
+            //    currentPosition = futurePosition;
+            //}
+        }
+
+        public void moveCamera(Direction direction)
+        {
+            switch (direction)
+            {
+                case Direction.LEFT:
+                    futurePosition = new Vector2(currentPosition.X - Constants.SCREEN_WIDTH, currentPosition.Y);
+                    break;
+                case Direction.RIGHT:
+                    futurePosition = new Vector2(currentPosition.X + Constants.SCREEN_HEIGHT, currentPosition.Y);
+                    break;
+                case Direction.UP:
+                    futurePosition = new Vector2(currentPosition.X, currentPosition.Y - Constants.SCREEN_HEIGHT);
+                    break;
+
+                case Direction.DOWN:
+                    futurePosition = new Vector2(currentPosition.X, currentPosition.Y + Constants.SCREEN_HEIGHT);
+                    break;
+            }
+        }
+
+        public bool isInScreen(Vector2 vector)
+        {
+            return ((vector.X > currentPosition.X - 16 && vector.X < currentPosition.X + Constants.SCREEN_WIDTH + 16) &&
+                    (vector.Y > currentPosition.Y - 16 && vector.Y < currentPosition.Y + Constants.SCREEN_HEIGHT + 16));
+        }
+
+        public Vector2 WorldToScreenPosition(Vector2 position)
+        {
+            return new Vector2(position.X - currentPosition.X, position.Y - currentPosition.Y);
+        }
+
+
+        //public bool MouseInsideWindow()
+        //{
+        //    var state = Mouse.GetState();
+        //    var pos = new Point(state.X, state.Y);
+        //    return pos.X >= 0 && pos.X <= Constants.SCREEN_WIDTH && pos.Y >= 0 && pos.Y <= Constants.SCREEN_HEIGHT;
+        //}
+    }
+}
