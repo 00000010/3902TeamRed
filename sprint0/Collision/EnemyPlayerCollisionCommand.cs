b4a06efdf225392095f184ed312e14bdaf1f390d
owenhennessey <obuddyblueandwhite@gmail.com>
2022-11-09 14:26:57 -0500

    Fixed my folder path

---
+++ sprint0/Collision/EnemyPlayerCollisionCommand.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vector2 = Microsoft.Xna.Framework.Vector2;
+
+namespace sprint0
+{
+    internal class EnemyPlayerCollisionCommand : ICommand
+    {
+        IObject enemy;
+        IObject player;
+        string intersectionLoc;
+        GameObjectManager manager;
+        public EnemyPlayerCollisionCommand(IObject player, IObject enemy, string intersectionLoc, GameObjectManager manager)
+        {
+            this.enemy = enemy;
+            this.player = player;
+            this.intersectionLoc = intersectionLoc;
+            this.manager = manager;
+        }
+
+        public void Execute()
+        {
+            Enemy enemy = (Enemy)this.enemy;
+            if (intersectionLoc.Contains("up"))
+            {
+                if (enemy.Velocity.Y > 0)  // if the velocity is directed towards the block
+                {
+                    enemy.Position -= new Vector2(0, 10);
+                }
+            }
+            if (intersectionLoc.Contains("down"))
+            {
+                if (enemy.Velocity.Y < 0)
+                {
+                    enemy.Position += new Vector2(0, 10);
+                }
+            }
+            if (intersectionLoc.Contains("left"))
+            {
+                if (enemy.Velocity.X < 0)
+                {
+                    enemy.Position -= new Vector2(10, 0);
+                }
+            }
+            if (intersectionLoc.Contains("right"))
+            {
+                if (enemy.Velocity.X > 0)
+                {
+                    enemy.Position += new Vector2(10, 0);
+                }
+            }
+        }
+    }
+}
---
+++ sprint0/Game/LoadRoomCommand.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace sprint0
+{
+    public class LoadRoomCommand : ICommand
+    {
+        private Game1 game;
+        private LevelLoader loader;
+        private Room nextRoom;
+        private Direction direction;
+        public LoadRoomCommand(Game1 game, Direction direction)
+        {
+            this.game = game;
+            this.direction = direction;
+        }
+
+        public void Execute()
+        {
+            loader = game.loader;
+            Room nextRoom = getRoom();
+            loader.ChangeRooms(nextRoom);
+        }
+
+        public Room getRoom()
+        {
+            loader = game.loader;
+            nextRoom = null;
+
+            //ToDo - data drive this switch
+            switch (direction)
+            {
+                case Direction.LEFT:
+                    nextRoom = loader.currentRoom.westRoomPtr;
+                    break;
+                case Direction.UP:
+                    nextRoom = loader.currentRoom.northRoomPtr;
+                    break;
+                case Direction.RIGHT:
+                    nextRoom = loader.currentRoom.eastRoomPtr;
+                    break;
+                case Direction.DOWN:
+                    nextRoom = loader.currentRoom.southRoomPtr;
+                    break;
+            }
+            return nextRoom;
+        }
+    }
+}
